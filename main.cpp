824555816c2ca04bdee4eea70cc2ec43c54ce844
LukArty <artemlukashov2002@gmail.com>
2022-12-14 23:17:07 +0700

    Полный перечень функций для проверки со стендом

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
+#include <unistd.h>
 #include <conserial.h>
-
+using namespace hwe;
 int main()
 {
 
-     hwe::Conserial forexample;
-     return 0;
-}
 
+    Conserial example;
+
+    example.Init();
+    example.InitByButtons();
+    example.Sendmessage({1,1,1,1},25);
+    example.SetPlateAngle(1,40);
+
+    example.GetPlatesAngles();
+    example.GetStartPlatesAngles();
+
+    //Состояние лазера
+    example.SetLaserState(1);
+    example.GetLaserState();
+    example.SetLaserState(0);
+    example.GetLaserState();
+    example.GetMaxLaserPower();
+
+    example.GetSignalLevels();
+    example.GetLightNoises();
+    example.GetMaxSignalLevels();
+    example.GetRotateStep();
+    example.GetErrorCode();
+    example.RunTest(1);
+
+    //Мощность лазера
+    example.SetLaserPower(0);
+    example.GetLaserPower();
+    example.SetLaserPower(50);
+    example.GetLaserPower();
+
+
+
+    return 0;
+}
